bd4ea0c28a3de9f627d6fc51e24774bb143f6f4a
Sirajuddin Asjad <sirajuddin@softwarehuset.no>
2024-03-09 15:43:12 +0100

    Case rename

---
+++ include/core.hpp
@@ -0,0 +1,22 @@
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include <iostream>
+#include <string>
+#include <thread>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <cstdint>
+#include <sstream>
+#include <filesystem>
+#include <variant>
+#include <bitset>
+#include <ctime>
+#include <exception>
+
+// Global variables
+#include "export.hpp"
+
+#endif
